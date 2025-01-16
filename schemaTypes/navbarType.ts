c3098e60427026283eb4d4dc31018f963f4a6c11
mowo <toryvick20@gmail.com>
2025-01-16 20:02:12 +0100

    Added logo name

--- schemaTypes/navbarType.ts
+++ schemaTypes/navbarType.ts
@@ -1,22 +1,26 @@
-import { defineField, defineType } from "sanity";
+import {defineField, defineType} from 'sanity'
 
 export const navBarType = defineType({
-    name: 'component',
-    title: 'Navbar',
-    type: 'document',
-    fields: [
-        defineField({
-            name: 'logo',
-            type: 'image',
-        }),
-        defineField({
-            name: 'navlinks',
-            type: 'array',
-            of: [{type: 'string'}]
-        }),
-        defineField({
-            name: 'call_to_action',
-            type: 'string',
-        })
-    ]
+  name: 'component',
+  title: 'Navbar',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'logo',
+      type: 'image',
+    }),
+    defineField({
+        name: 'logo_name',
+        type: 'string',
+      }),
+    defineField({
+      name: 'navlinks',
+      type: 'array',
+      of: [{type: 'string'}],
+    }),
+    defineField({
+      name: 'call_to_action',
+      type: 'string',
+    }),
+  ],
 })
